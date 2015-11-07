cf5201c978e26c1d6cf2ea802fefc66acb7236bd
lix1@umbc.edu <lix1@umbc.edu>
2015-11-07 13:49:56 -0500

    datatables

--- public/js/controllers/console_hospital.js
+++ public/js/controllers/console_hospital.js
@@ -1,11 +1,19 @@
 'use strict';
 
-angular.module('app.consoleHospitalCtrl', [])
+angular.module('app.consoleHospitalCtrl', ['datatables'])
   .controller('ClassroomCtrl', ['$state','$stateParams', '$scope', '$http','$modal', function($state,$stateParams, $scope, $http,$modal) {
       console.log($stateParams.classroomId)
       $scope.classroom = {};
       $scope.classroom.id=1;
       $scope.classroom.title="CS446";
       $scope.classroom.name="Machine Learning";
+        $scope.classroom.queations=[];
+      for (var i=0;i<10;i++){
+          var question = {};
+          question.votes=i*3;
+          question.answerCount=i*2;
+          question.title="question " + i;
+          $scope.classroom.queations.push(question)
+      }
   }])
 ;
